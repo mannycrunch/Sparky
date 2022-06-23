56136911077a63211385c61bec6c7f742f2b7def
Manny Lopez <manuell@midland.edu>
2022-06-23 17:28:08 -0500

    Section 4 - Lesson 35 done

--- SparkyNUnitTest/CalculatorNUnitTests.cs
+++ SparkyNUnitTest/CalculatorNUnitTests.cs
@@ -1,20 +1,19 @@
-﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sparky
+﻿namespace Sparky
 {
     [TestFixture]
     public class CalculatorNUnitTests
     {
+        private Calculator calc;
+        [SetUp]
+        public void Setup()
+        {
+            calc = new Calculator();
+        }
+
         [Test]
         public void AddNumbers_InputTwoInt_GetCorrectAddition()
         {
             //Arrange
-            Calculator calc = new();
 
             //Act
             int result = calc.AddNumbers(10, 20);
@@ -26,8 +25,6 @@ namespace Sparky
         [Test]
         public void IsOddNumber_InputEvenNumber_ReturnFalse()
         {
-            Calculator calc = new();
-
             bool isOdd = calc.IsOddNumber(10);
             Assert.That(isOdd, Is.False);
             Assert.IsFalse(isOdd);
@@ -38,8 +35,6 @@ namespace Sparky
         [TestCase(13)]
         public void IsOddNumber_InputOddNumber_ReturnTrue(int a)
         {
-            Calculator calc = new();
-
             bool isOdd = calc.IsOddNumber(a);
             Assert.That(isOdd, Is.True);
             Assert.IsTrue(isOdd);
@@ -50,7 +45,6 @@ namespace Sparky
         [TestCase(11, ExpectedResult = true)]
         public bool IsOddNumber_InputNumber_ReturnTrueIfOdd(int a)
         {
-            Calculator calc = new();
             return calc.IsOddNumber(a);
         }
 
@@ -61,13 +55,33 @@ namespace Sparky
         public void AddNumbersDouble_InputTwoDouble_GetCorrectAddition(double a, double b)
         {
             //Arrange
-            Calculator calc = new();
 
             //Act
             double result = calc.AddNumbersDouble(a, b);
 
             //Assert
             Assert.AreEqual(15.9, result, .2); // 15.7 - 16.1
+        }
+
+        [Test]
+        public void OddRanger_InputMinAndMaxRange_ReturnsValidOddNumberRange()
+        {
+            //Arrange
+            List<int> expectedOddRange = new() { 5, 7, 9 }; //5-10
+
+            //Act
+            List<int> result = calc.GetOddRange(5, 10);
+
+            //Assert
+            Assert.That(result, Is.EquivalentTo(expectedOddRange));
+            //Assert.AreEqual(expectedOddRange, result);
+            //Assert.Contains(7, result);
+            Assert.That(result, Does.Contain(7));
+            Assert.That(result, Is.Not.Empty);
+            Assert.That(result.Count, Is.EqualTo(3));
+            Assert.That(result, Has.No.Member(6));
+            Assert.That(result, Is.Ordered);
+            Assert.That(result, Is.Unique);
         }
     }
 }
